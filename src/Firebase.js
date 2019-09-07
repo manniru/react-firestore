7d0b3e3be15520b3e7e19637934423a99a5b1e8e
Mannir <manniru@gmail.com>
2019-09-07 07:34:33 +0100

    Add Offline Capability

--- src/Firebase.js
+++ src/Firebase.js
@@ -16,4 +16,22 @@ firebase.initializeApp(config);
 
 firebase.firestore().settings(settings);
 
+firebase.firestore().enablePersistence()
+  .catch(function(err) {
+      if (err.code == 'failed-precondition') {
+          console.log('failed-precondition')
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          // ...
+      } else if (err.code == 'unimplemented') {
+          console.log('unimplemented')
+          // The current browser does not support all of the
+          // features required to enable persistence
+          // ...
+      }
+  });
+// Subsequent queries will use persistence, if it was enabled successfullytest.firestore.js
+
+  
+
 export default firebase;
